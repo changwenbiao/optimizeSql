3b414ca1a8fd845656bad82b1f4326961959fcc0
changwenbiao <422002381@qq.com>
2020-07-19 14:34:38 +0800

    first commit

---
+++ 查询需要索引的列.sql
@@ -0,0 +1,24 @@
+select owner, column_name, num_rows, cardinality, selectivity
+  from (select b.owner,
+               a.column_name,
+               b.num_rows,
+               a.num_distinct cardinality,
+               round(a.num_distinct / b.num_rows * 100， 2) selectivity
+          from dba_tab_col_statistics a, dba_tables b
+         where a.owner = b.owner
+           and a.table_name = b.table_name
+           and a.owner = 'scott'
+           and a.table_name = 'test')
+ where selectivity > 20
+   and column_name not in (select column_name
+                             from dba_ind_columns
+                            where table_owner = 'scott'
+                              and table_name = 'test')
+   and column_name in
+       (select c.name
+          from sys.col_usage$ u, sys.obj$ o, sys.col$ c, sys.user$ r
+         where o.obj# = u.obj#
+           and c.obj# = u.obj#
+           and c.col# = u.intcol#
+           and r.name = 'scott'
+           and o.name = 'test')
